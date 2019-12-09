a5f0c8192283552ad28232cfffb5192275ab8a9e
shitashige <t_shitashige@ddthink.com>
2019-12-09 21:30:22 +0900

    園主1-6途中。

---
+++ 第1章/演習1-6/Exercises1-6.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file Exerises1-6.cpp
+ * @brief 演習1-6	List1-7のプログラムを変更して、少数日を持つ実数値をxやyに代入するプログラムを作成せよ。
+					その実行結果から、int型変数が整数値のみしか扱えない事を確認する事。
+ * @author shitashige
+ * @date 20191204
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+ /**
+  * @fn
+  * メイン関数
+  * @brief int型は少数点しか扱えない事を認知するプログラム。
+  * @return 0:正常終了
+  */
+int main() {
+
+	int firstInt = 63.9;	// 一つ目のint型を63.9にする。
+	int secondInt = 18.4;	// 二つ目のint型を18.4にする。
+
+	float firstFloat = 63.9;	// 一つ目のfloat型を63.9にする。
+	float secondFloat = 18.4;		// 二つ目のfloat型を18.4にする。
+
+
+	
+
+}
