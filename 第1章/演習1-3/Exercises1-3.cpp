65512b54a7c00306ff0fefc1c4ac48d3722de006
shitashige <t_shitashige@ddthink.com>
2019-12-09 20:05:53 +0900

    演習問題1-3終了。

---
+++ 第1章/演習1-3/Exercises1-3.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file Exerises.cpp
+ * @brief 演習1-3　文の終端を示すセミコロン；が欠如しているとどうなるか。プログラムをコンパイルして検証せよ。
+ * @author shitashige
+ * @date 20191209
+ */
+
+#include <iostream>
+
+/**
+ * @fn
+ * メイン関数
+ * @brief セミコロンが欠如している場合の動作確認
+ * @return 0:正常終了
+ */
+int main() {
+	std::cout << "aaa"; 
+
+	/* セミコロンが無いと、構文エラーが発生する。 */
+//	std::cout << "aaa"
+
+	return 0;
+}
+
