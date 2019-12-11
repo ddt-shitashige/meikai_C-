6ddb68400673f86d4008a6527fa978dbd56ba4fd
shitashige <t_shitashige@ddthink.com>
2019-12-11 20:56:23 +0900

    一旦コミット。

---
+++ 第1章/演習1-11/Exercises1-11.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file Exerises1-11.cpp
+ * @brief 演習1-11	右に示すように、二つの実数値を読み込み、その合計と平均を求めて表示するプログラムを作成せよ。
+ * @author shitashige
+ * @date 20191211
+ */
+
+
+/**
+ * @brief main関数
+ * @return 0:正常
+ */
+int main() {
+
+
+
+}
