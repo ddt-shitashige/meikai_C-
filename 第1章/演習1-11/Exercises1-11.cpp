f99a90c40f763fd66778bbb3706ad6b6b88e57ea
shitashige <t_shitashige@ddthink.com>
2019-12-11 21:06:51 +0900

    演習1-11完了。

--- 第1章/演習1-11/Exercises1-11.cpp
+++ 第1章/演習1-11/Exercises1-11.cpp
@@ -5,13 +5,33 @@
  * @date 20191211
  */
 
+#include <iostream>
 
+using namespace std;
 /**
  * @brief main関数
  * @return 0:正常
  */
 int main() {
 
+	double inputNumberX = 0;	// 入力したXの値
+	double inputNumberY = 0;	// 入力したYの値
 
+	/* X値の入力を促す分を出力 */
+	cout << "Xの値を入力してください\n";
+	
+	/* X値入力*/
+	cin >> inputNumberX;
+
+	/* Y値の入力を促す分を出力 */
+	cout << "Yの値を入力してください\n";
+
+	/* Y値入力*/
+	cin >> inputNumberY;
+
+	/* 合計値と平均値出力 */
+	cout << "合計は" << inputNumberX + inputNumberY << "です。\n平均は" << (inputNumberX + inputNumberY) / 2 << "です";
+
+	return 0;
 
 }
