35ec067bf55994fb79427bc305b68d7f81cc1af5
shitashige <t_shitashige@ddthink.com>
2020-01-15 20:36:32 +0900

    演習3-11完了致しました。

--- 第3章/演習3-11/Exercises3-11.cpp
+++ 第3章/演習3-11/Exercises3-11.cpp
@@ -1,7 +1,19 @@
+/**
+ * @file Exerises3-11.cpp
+ * @brief 演習3-11	1からnまでの和を求めるList3-9をfor文で作成せよ。
+ * @author shitashige
+ * @date 20200115
+ */
+
 #include <iostream>
 
 using namespace std;
-
+/**
+ * @fn
+ * メイン関数
+ * @brief 1から入力値までの総和を求める関数
+ * @return 0:正常終了
+ */
 int main() {
 	int n;		// 整数n（問題文がこうしているだけで私の意志ではない）
 
@@ -16,7 +28,7 @@ int main() {
 	} while (n <= 0);
 
 	int sum = 0;		// 合計値(問題文がこうしているだけで私の意志ではない)
-	
+
 	/* iをインクリメントしていってそれを足すことで合計値を求める */
 	for (int i = 1; i <= n; i++) {
 		/* sumにiを足す */
