0ea871696a6cc3a0c8149437ae4b0891bcb69523
jitaku <@mail.com>
2020-01-23 20:56:32 +0900

    演習3-20完了しました。

--- 第3章/演習3-20/Exercises3-20.cpp
+++ 第3章/演習3-20/Exercises3-20.cpp
@@ -1,6 +1,7 @@
 /**
  * @file Exerises3-20.cpp
- * @brief	
+ * @brief	List3-4は左下が直角の直角三角形を表示するプログラムであった。
+ 			直角が左上側、右上側右下側の直角三角形を表示するプログラムをそれぞれ作成せよ
  * @author shitashige
  * @date 20200123
  */
@@ -22,6 +23,23 @@ int main() {
 	/* 数値入力 */
 	std::cin >> inputCount;
 
+	/* 文字出力 */
+	std::cout << "左上\n\n";
+
+	/* 入力数の列分ループする */
+	for (int i = 0; i < inputCount; i++) {
+		/* 入力数の行分ループする しかし、初期値をiにすることにより、出力する数を1つずつ減らす */
+		for (int j = i; j < inputCount; j++) {
+			/* 文字出力 */
+			std::cout << "*";
+	}
+		/* 改行 */
+		std::cout << "\n";
+	}
+
+	/* 文字出力 */
+	std::cout << "\n\n\n右上\n\n";
+
 	/* 入力数の列分ループする */
 	for (int i = 0; i < inputCount; i++) {
 		/* 入力数の行分ループする */
@@ -41,4 +59,28 @@ int main() {
 		/* 改行 */
 		std::cout << "\n";
 	}
+
+	/* 文字出力 */
+	std::cout << "\n\n\n右下\n\n";
+
+	/* 入力数の列分ループする */
+	for (int i = 1; i <= inputCount; i++) {
+		/* 入力数の行分ループする */
+		for (int j = 1; j <= inputCount; j++) {
+			/* 入力した文字よりiを引くことにより、右下が直角な三角形を作成できる。 */
+			if (j > inputCount - i) {
+				/* 文字出力 */
+				std::cout << "*";
+			}
+			/* 列数が行数を超えていたら空白を出力 */
+			else {
+				/* 文字出力 */
+				std::cout << " ";
+
+			}
+		}
+		/* 改行 */
+		std::cout << "\n";
+	}
+
 }
