1fbe77e6b0dcedc7636e0471f5ca00de36d7dbe4
jitaku <@mail.com>
2020-03-23 14:05:48 +0900

    演習6-16完了しました。

--- 第6章/演習6-16/演習6-16/Exercises6-16.cpp
+++ 第6章/演習6-16/演習6-16/Exercises6-16.cpp
@@ -8,35 +8,35 @@
 
 #include <iostream>
 
-/**
- * @fn
- * main
- * @brief main関数
- * @return 0:正常終了
- */
+ /**
+  * @fn
+  * main
+  * @brief main関数
+  * @return 0:正常終了
+  */
 int main() {
 
 	const int element = 5;	// 要素数
 	static int staticArray[5];	// 性的記憶期間を持つ配列
-    int Array[5];	// 自動記憶期間を持つ配列
+	int Array[5];	// 自動記憶期間を持つ配列
 
 
-    /* メッセージ出力 */
-    std::cout << "静的記憶期間の場合\n";
+	/* メッセージ出力 */
+	std::cout << "静的記憶期間の場合\n";
 
 	/* 配列の中身出力 */
-	for (int i=0; i < element; i++) {
+	for (int i = 0; i < element; i++) {
 		/* メッセージ出力 */
-		std::cout<<"["<<i<<"]="<<staticArray[i]<<" \n";
+		std::cout << "[" << i << "]=" << staticArray[i] << " \n";
 	}
-    /* メッセージ出力 */
-    std::cout << "自動記憶期間の場合\n";
+	/* メッセージ出力 */
+	std::cout << "自動記憶期間の場合\n";
 
-    /* 配列の中身出力 */
-    for (int i = 0; i < element; i++) {
-        /* メッセージ出力 */
-        std::cout << "[" << i << "]=" << Array[i] << " \n";
-    }
+	/* 配列の中身出力 */
+	for (int i = 0; i < element; i++) {
+		/* メッセージ出力 */
+		std::cout << "[" << i << "]=" << Array[i] << " \n";
+	}
 
 	return 0;
 }
