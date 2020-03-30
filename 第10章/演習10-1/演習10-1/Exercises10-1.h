046277d5ecb81a2e8bc87a26ac19e692481c327a
jitaku <@mail.com>
2020-03-30 14:48:04 +0900

    演習10-1完了しました。

---
+++ 第10章/演習10-1/演習10-1/Exercises10-1.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+class Human {
+	private:
+		std::string name;	// –¼‘O
+		double height;	// g’·
+		double weight;	// ‘Ìd
+
+	public:
+		std::string GetName();
+		double GetHeight();
+		double GetWeight();
+		void SetName(std::string name);
+		void SetHeight(double height);
+		void SetWeight(double weight);
+
+};
